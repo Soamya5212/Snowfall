d167b855720350a9ecd137c7d600946c8f38bbd6
Soamya5212 <76622113+Soamya5212@users.noreply.github.com>
2021-06-28 21:22:09 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,55 @@
-var backgroundImage;
-var snowI;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
 
-function preload() {
-  backgroundImage = loadImage("snow3.jpg");
-  snowI = loadImage("snow4.webp");
+var snow=[];
+var maxSnow=30;
+var snowfall;
+var bg;
+//var snowF1,snowF2,snowflake;
+//var snowFCreatedFrame = 0;
+
+
+function preload(){
+    
+  bg = loadImage("snow4.png");
+  snowF1 = loadImage("snow4.webp");
+  snowF2 = loadImage("snow4.webp");
+
 }
 
+
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(600, 450);
+
+  engine = Engine.create();
+
+	world = engine.world;
+ 
+  if(frameCount%150 === 0){
+    for(var i=0; i<maxSnow; i++){
+        snow.push( new Snow(random(0,600), random(0,450) ,7));
+      }
+    }
+
+Engine.run(engine);
+
+
 }
 
 function draw() {
-  background(backgroundImage);
-  createSnow();
+  background(bg);  
+
+  for(var i =0;i<maxSnow;i++){
+   snow[i].display();
+   snow[i].update();
+    }
+
+   
+
+
   drawSprites();
 }
-function createSnow(){
-if (frameCount % 3 === 0) {
- 
-    var snow = createSprite(random(0,800),0);
-    snow.addImage(snowI);
-    snow.scale = 0.09
-    snow.velocityY = random(2,5)
-    snow.lifetime = 150
-  }
-}
-
-
